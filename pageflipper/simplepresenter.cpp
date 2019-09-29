cce82136419a3608de51366d92f206226c276fd9
kmgnkhr <49336378+kmgnkhr@users.noreply.github.com>
2019-09-29 17:48:14 +0900

    Update UI
    Low Batt -> turn LED on.

--- pageflipper/simplepresenter.cpp
+++ pageflipper/simplepresenter.cpp
@@ -4,26 +4,60 @@
 void SimplePresenter::begin()
 {
     _led.begin();
+    _isLowBatt = false;
+    _led_state = -1;
 }
 
 void SimplePresenter::loop()
 {
+    const bool lowBatt = M5.Axp.GetWarningLeve() == 1;
+    if (_isLowBatt != lowBatt) {
+        _isLowBatt = lowBatt;
+        if (_isLowBatt) {
+            _led.turnOn();
+        } else if (_led_state < 0) {
+            _led.turnOff();
+        } else if (_led_state == 0) {
+            _led.turnOn();
+        } else {
+            _led.blink(static_cast<uint32_t>(_led_state));
+        }
+    }
     _led.loop();
 }
 
 void SimplePresenter::enterStandby()
 {
     M5.Axp.ScreenBreath(0);
+    turnOffLED();
 }
 
 void SimplePresenter::enterAdvertising()
 {
     M5.Axp.ScreenBreath(12);
-    _led.blink(1000);
+    blinkLED(1000);
 }
 
 void SimplePresenter::enterKeyboard()
 {
     M5.Axp.ScreenBreath(0);
-    _led.turnOff();
+    turnOffLED();
+}
+
+void SimplePresenter::turnOnLED()
+{
+    _led_state = 0;
+    _led.turnOn();
+}
+
+void SimplePresenter::turnOffLED()
+{
+    _led_state = -1;
+    if (!_isLowBatt) _led.turnOff();
+}
+
+void SimplePresenter::blinkLED(uint32_t interval_ms)
+{
+    _led_state = static_cast<int>(interval_ms);
+    if (!_isLowBatt) _led.blink(interval_ms);
 }
--- pageflipper/simplepresenter.h
+++ pageflipper/simplepresenter.h
@@ -7,6 +7,8 @@
 class SimplePresenter : public IPresenter
 {
     LED _led;
+    bool _isLowBatt;
+    int _led_state;
 public:
     virtual void begin();
     virtual void loop();
@@ -14,6 +16,10 @@ public:
     virtual void enterStandby();
     virtual void enterAdvertising();
     virtual void enterKeyboard();
+private:
+    void turnOnLED();
+    void turnOffLED();
+    void blinkLED(uint32_t interval_ms);
 };
 
 #endif //ifndef SIMPLEPRESENTER_H__
